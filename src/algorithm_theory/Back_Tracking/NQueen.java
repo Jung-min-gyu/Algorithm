3ae88db5901b6d4d79d599045a50c3930ce2b27d
Jungmingyu <vlxjalsrb@naver.com>
2022-06-27 11:47:24 +0900

    12 day

---
+++ src/algorithm_theory/Back_Tracking/NQueen.java
@@ -0,0 +1,36 @@
+package algorithm_theory.Back_Tracking;
+
+import java.util.ArrayList;
+
+public class NQueen {
+    public boolean isAvailable(ArrayList<Integer> candidate, Integer currentCol) {
+        Integer currentRow = candidate.size();
+        for (int index = 0; index < currentRow; index++) {
+            if ((candidate.get(index) == currentCol) ||
+                    (Math.abs(candidate.get(index) - currentCol) == currentRow - index)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void dfsFunc(Integer N, Integer currentRow, ArrayList<Integer> currentCandiate) {
+        if (currentRow == N) {
+            System.out.println(currentCandiate);
+            return ;
+        }
+
+        for (int index = 0; index < N; index++) {
+            if (isAvailable(currentCandiate, index) == true) {
+                currentCandiate.add(index);
+                this.dfsFunc(N, currentRow + 1, currentCandiate);
+                currentCandiate.remove(currentCandiate.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        NQueen nObject = new NQueen();
+        nObject.dfsFunc(4, 0, new ArrayList<Integer>());
+    }
+}
